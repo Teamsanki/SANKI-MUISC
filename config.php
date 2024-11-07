3ae184980104783f44cf1e3cf95b9e92e5ee85ff
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-11-07 07:31:12 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '__-B3');
-if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'Was_B3');
-if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '5579729798');
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'SANKIXD');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'TSGCODER');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '7759282826');
 
 if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
-if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'BravCc_Bot');
-if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7048214022:AAGS75gaumDRfGtOOXb4YphshInKf9KzArw');
-if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-4194518429');
-if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002021756101');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'XCARDLIVEBOT');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7790830566:AAEKrFbKPcM_p4BgOJgjigrnbdwMQnR_rWc');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002100433415');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002355682727');
 
 if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
 if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
